1bf8f94930bacbe971a481b15e476b6eb126f7b9
Ngor Thiam <thiam.ngor@gmail.com>
2025-12-31 00:13:55 +0100

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: (import.meta as any).env?.VITE_SUPABASE_URL || '',
-  supabaseAnonKey: (import.meta as any).env?.VITE_SUPABASE_ANON_KEY || ''
+  supabaseUrl: 'https://morwnomfnfhblqnctlyq.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vcndub21mbmZoYmxxbmN0bHlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxNDAxMjUsImV4cCI6MjA4MTcxNjEyNX0.dsrJ4QpP5F_9P55-gt6e4PquMg9QykY6mFU8HClQrYs'
 };
