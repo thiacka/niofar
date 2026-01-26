287835e59ac1c93a6ea17f9835a49b34e70a825d
Ngor THIAM <thiam.ngor@gmail.com>
2026-01-26 21:22:30 +0100

    remove old supabase function

--- supabase/migrations/20260113185212_force_schema_reload.sql
+++
@@ -1,9 +0,0 @@
-/*
-  # Force Schema Cache Reload
-  
-  This migration forces PostgREST to reload its schema cache.
-*/
-
-SELECT pg_notify('pgrst', 'reload schema');
-
-COMMENT ON TABLE rentals IS 'Equipment rentals and incentive services';
