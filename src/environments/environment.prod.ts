d1fd0720ee03bbd142c2798a58c25ee0fcb7be1e
Ngor THIAM <thiam.ngor@gmail.com>
2026-04-25 23:42:41 +0200

    add cloudinary to manager attachements

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   supabaseUrl: 'https://ukecyldurjcnpcxdhfcc.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrZWN5bGR1cmpjbnBjeGRoZmNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0NTEzNDYsImV4cCI6MjA4NTAyNzM0Nn0.FIQb5p-WO1EXiqi0vswi_Xw1hXZp2j7byfjKmEUGy3g',
   siteUrl: 'https://niofartourisme.com',
-  cloudinaryCloudName: 'YOUR_CLOUD_NAME',
-  cloudinaryUploadPreset: 'YOUR_UPLOAD_PRESET'
+  cloudinaryCloudName: 'duuvvtr7k',
+  cloudinaryUploadPreset: 'nio-far-tourisme'
 };
