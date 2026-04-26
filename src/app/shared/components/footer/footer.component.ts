9230e9176378b6196b5dfd4bac489a5f9569875f
Ngor THIAM <thiam.ngor@gmail.com>
2026-04-26 02:12:29 +0200

    fix email contact

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -64,7 +64,7 @@ import { LanguageService } from '../../../core/services/language.service';
               </svg>
               <span>+221 711 52 54 36</span>
             </a>
-            <a href="mailto:contact@niofar.com" class="contact-item">
+            <a href="mailto:contact@niofartourisme.com" class="contact-item">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/>
                 <polyline points="22,6 12,13 2,6"/>
